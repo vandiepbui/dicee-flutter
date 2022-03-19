293d3c63af3a6e0aba8fcec6e8d56648c4f36769
Bui Van Diep <35633954+vandiepbui@users.noreply.github.com>
2022-03-19 20:36:53 +0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
+          centerTitle: true,
           title: Text('Dicee'),
           backgroundColor: Colors.red,
         ),
@@ -15,9 +19,45 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var leftDice = 1;
+  var rightDice = 1;
+
+  void onChangeDiceValues() {
+    setState(() {
+      leftDice = Random().nextInt(6) + 1;
+      rightDice = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                onChangeDiceValues();
+              },
+              child: Image.asset('images/dice$leftDice.png'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                onChangeDiceValues();
+              },
+              child: Image.asset('images/dice$rightDice.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
